4266becf750923b92ada7fb8bf209be4ba69a855
FaRaZ-32729 <sial32729@gmail.com>
2025-12-24 22:01:24 +0500

    port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ dotenv.config();
 dbConnection();
 // const port = 5050;
 // const port = 5000;
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5051;
 const app = express();
 const server = http.createServer(app);
 
